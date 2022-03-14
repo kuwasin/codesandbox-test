39c0da6383dda99cb9f70a644c9e59c97d5d826f
kuwasin <76948644+kuwasin@users.noreply.github.com>
2022-03-14 23:24:23 +0900

    2022/3/14/23:24

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const onClickAdd = () => {
 
   //li生成
   const li = document.createElement("li");
-
+  
   //div生成
   const div = document.createElement("div");
   div.className = "list-row";
@@ -18,11 +18,36 @@ const onClickAdd = () => {
   const compliteButton = document.createElement("button");
   compliteButton.innerText = "完了";
   compliteButton.addEventListener("click", () => {
-    //完了リストに追加するリスト
+     //押された完了ボタンの親タグを未完了リストから削除
+     deleteFromIncompleteList(li);
+    //完了リストに追加する要素
     const addTarget = compliteButton.parentNode;
+   
+   //TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+    
+    //div以下を初期化
+    addTarget.textContent = null;
+    
+   //liタグの作成
+   const liComplete = document.createElement("li");
+    //pタグを生成
+    const p = document.createElement("p");
+    p.innerText = text;
+    //buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    //liの子要素にdivタグを設定
+    liComplete.appendChild(addTarget);
+    //divタグの子要素に各要素を設定
+    addTarget.appendChild(p);
+    addTarget.appendChild(backButton);
     console.log(addTarget);
-    //押された完了ボタンの親タグを未完了リストから削除
-    deleteFromIncompleteList(compliteButton.parentNode);
+
+    //完了リスtに追加
+    document.getElementById("complete-list").appendChild(liComplete);
+
   });
 
   //button(削除)タグ生成
@@ -30,11 +55,10 @@ const onClickAdd = () => {
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     //押された削除ボタンの親タグを未完了リストから削除
-    deleteFromIncompleteList(deleteButton.parentNode);
+    deleteFromIncompleteList(li);
   });
 
   li.appendChild(div);
-
   //divタグの子要素に各要素を設定
   div.appendChild(p);
   // li.appendChild(p);
@@ -42,7 +66,7 @@ const onClickAdd = () => {
   div.appendChild(deleteButton);
 
   //未完了リストに追加
-  document.getElementById("incomplete-list").appendChild(div);
+  document.getElementById("incomplete-list").appendChild(li);
 
   //未完了リストから指定の要素を削除
   const deleteFromIncompleteList = (target) => {
