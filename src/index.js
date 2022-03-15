c4d86df8749a14b7a8bc559d4682e011c2e86356
kuwasin <76948644+kuwasin@users.noreply.github.com>
2022-03-15 23:18:55 +0900

    2022/3/15

--- src/index.js
+++ src/index.js
@@ -3,51 +3,71 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+//未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+//未完了リストに追加する関数
+const createIncompleteList = (text) => {
   //li生成
   const li = document.createElement("li");
-  
+
   //div生成
   const div = document.createElement("div");
   div.className = "list-row";
 
   //pタグ生成
   const p = document.createElement("p");
-  p.innerText = inputText;
+  p.innerText = text;
 
   //button(完了)タグ生成
   const compliteButton = document.createElement("button");
   compliteButton.innerText = "完了";
   compliteButton.addEventListener("click", () => {
-     //押された完了ボタンの親タグを未完了リストから削除
-     deleteFromIncompleteList(li);
+    //押された完了ボタンの親タグを未完了リストから削除
+    deleteFromIncompleteList(li);
     //完了リストに追加する要素
     const addTarget = compliteButton.parentNode;
-   
-   //TODO内容テキストを取得
+
+    //TODO内容テキストを取得
     const text = addTarget.firstElementChild.innerText;
-    
+
     //div以下を初期化
     addTarget.textContent = null;
-    
-   //liタグの作成
-   const liComplete = document.createElement("li");
+
+    //liタグの作成
+    const liToComplete = document.createElement("li");
     //pタグを生成
     const p = document.createElement("p");
     p.innerText = text;
+
     //buttonタグ生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      //押された押すボタンの親タグを完了リストから削除
+      // const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(liToComplete);
+      // document.getElementById("complete-list").removeChild(deleteTarget);
+
+      //テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
 
     //liの子要素にdivタグを設定
-    liComplete.appendChild(addTarget);
+    liToComplete.appendChild(addTarget);
     //divタグの子要素に各要素を設定
     addTarget.appendChild(p);
     addTarget.appendChild(backButton);
     console.log(addTarget);
 
     //完了リスtに追加
-    document.getElementById("complete-list").appendChild(liComplete);
-
+    document.getElementById("complete-list").appendChild(liToComplete);
   });
 
   //button(削除)タグ生成
@@ -67,11 +87,6 @@ const onClickAdd = () => {
 
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
-
-  //未完了リストから指定の要素を削除
-  const deleteFromIncompleteList = (target) => {
-    document.getElementById("incomplete-list").removeChild(target);
-  };
 };
 
 document.getElementById("add-btn").addEventListener("click", onClickAdd);
